72cb7fb471156164d0153eadb5b59d606b8ecfd1
miguelRivera130 <angel1301999@gmail.com>
2021-03-17 19:27:18 -0500

    creacion de segunda interaccion

---
+++ scripts/Current.js
@@ -0,0 +1,11 @@
+const services = document.querySelectorAll('.service');
+const thumbs = document.querySelectorAll('.service__thumb');
+let sizeId;
+
+//zoom img
+for (let i = 0; i < services.length; i++) {
+
+    let thumb = thumbs[i]
+    service = services[i]
+
+}

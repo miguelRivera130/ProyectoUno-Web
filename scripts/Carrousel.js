4ec36961e40ccac1a038b451d10d7adc29793b17
miguelRivera130 <angel1301999@gmail.com>
2021-03-17 19:18:50 -0500

    creacion de primera interaccion

---
+++ scripts/Carrousel.js
@@ -0,0 +1,12 @@
+//slide carrousel
+const items = document.querySelectorAll('.item');
+const sliders = document.querySelectorAll('.item__slider');
+let currentSlide = 0;
+let intervalId;
+
+for (let i = 0; i < items.length; i++) {
+
+    item = items[i];
+    let slider = sliders[i]
+
+}

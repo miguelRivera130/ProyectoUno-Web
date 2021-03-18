f3a5fbc528b6d7415bee17557c35f1e54c8f1e23
miguelRivera130 <angel1301999@gmail.com>
2021-03-17 19:20:27 -0500

    limitacion de primera interaccion

--- scripts/Carrousel.js
+++ scripts/Carrousel.js
@@ -9,4 +9,18 @@ for (let i = 0; i < items.length; i++) {
     item = items[i];
     let slider = sliders[i]
 
+    function handleMouseEnter() {
+        intervalId = setInterval(handleInterval, 1500);
+    }
+
+    item.addEventListener('mouseenter', handleMouseEnter);
+
+    function handleMouseLeave() {
+        clearInterval(intervalId);
+        currentSlide = 0;
+        slider.style.transform = 'translate( 0px, 0px)';
+    }
+
+    item.addEventListener('mouseleave', handleMouseLeave);
+
 }

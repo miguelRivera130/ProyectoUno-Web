638c588bb37745a98f80bc69eed1c8f73f52bb52
miguelRivera130 <angel1301999@gmail.com>
2021-03-17 19:29:04 -0500

    limitacion de segunda interaccion

--- scripts/Current.js
+++ scripts/Current.js
@@ -8,4 +8,17 @@ for (let i = 0; i < services.length; i++) {
     let thumb = thumbs[i]
     service = services[i]
 
+    function handleThumbEnter() {
+        sizeId = setInterval(handleZoomInterval, 0500);
+
+    }
+
+    service.addEventListener('mouseenter', handleThumbEnter);
+
+    function handleThumbLeave() {
+        clearInterval(sizeId);
+        thumb.style.transform = `scale( 1, 1)`
+    }
+
+    service.addEventListener('mouseleave', handleThumbLeave);
 }

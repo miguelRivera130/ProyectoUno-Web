976c0b8b3e8c91039e2744a646fc5110c3ff6ea6
miguelRivera130 <angel1301999@gmail.com>
2021-03-20 21:38:42 -0500

    tercera interaqccion finalizada

--- scripts/Current.js
+++ scripts/Current.js
@@ -2,7 +2,11 @@ const current = document.querySelector('.membership__current');
 const thumbs = document.querySelectorAll('.membership__thumb');
 const srcName = document.querySelector('.membership__name');
 const srcDescription = document.querySelector('.membership__description');
+const srcBenefitTwo = document.querySelector('.membership__benefitTwo');
+const srcBenefitThree = document.querySelector('.membership__benefitThree');
+const srcBenefitFour = document.querySelector('.membership__benefitFour');
 const img = document.querySelector('.membership__img');
+const button = document.querySelector('button--membership');
 let thumb;
 
 for (let i = 0; i < thumbs.length; i++) {
@@ -20,6 +24,9 @@ for (let i = 0; i < thumbs.length; i++) {
                 img.setAttribute('src', srcImg);
                 srcName.innerHTML = 'Membresia';
                 srcDescription.innerHTML = 'Conviértete en miembro para mantener una base de datos para la realización de múltiples proyectos con tus características y gustos además de conseguir descuentos en sesiones y estudios a la venta.';
+                srcBenefitTwo.innerHTML = 'Descuentos y prioridad en la organizacion de sesiones.';
+                srcBenefitThree.innerHTML = 'Disponivilidad en la seleccion de modelos.';
+                srcBenefitFour.innerHTML = 'Descuentos y acceso a la compra de proyectos limitados.';
                 break;
                 
             case "radioTwo":
@@ -27,6 +34,9 @@ for (let i = 0; i < thumbs.length; i++) {
                 img.setAttribute('src', srcImg);
                 srcName.innerHTML = 'Suscripción';
                 srcDescription.innerHTML = 'Suscribe tu correo electronico para enterarte de primera mano todas las novedades en cuanto a servicios y productos presentes en la pagina.';
+                srcBenefitTwo.innerHTML = '';
+                srcBenefitThree.innerHTML = '';
+                srcBenefitFour.innerHTML = '';
                 break;
 
         }

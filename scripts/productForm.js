507782d16434c5a66e000d131c363276a0a395b7
miguelRivera130 <angel1301999@gmail.com>
2021-04-23 00:57:48 -0500

    creacion de productForm

---
+++ scripts/productForm.js
@@ -0,0 +1,8 @@
+const productForm = document.querySelector('.productForm');
+
+productForm.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    console.log('hola');
+    
+});
